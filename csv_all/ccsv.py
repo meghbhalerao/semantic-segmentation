3fa5b7c225499141f6009f9199706288bae53ff5
Megh Bhalerao Megh.Bhalerao@Pennmedicine.upenn.edu <bhaleram@cubic-login1.uphs.upenn.edu>
2019-11-19 21:52:58 -0500

    Additional Commenting

--- csv_all/ccsv.py
+++ csv_all/ccsv.py
@@ -1,12 +1,13 @@
 import csv 
 import os
 import pandas as pd
-#Creates a CSV file in the same folder where the experiment is being carried out
-
+# This script creates 10 csv scripts as given in the readme description 
+# Path to folder where all the training datapoints are located
 train_path = "/cbica/home/bhaleram/comp_space/fets/data/PreProcessed_Data/train/"
 f1 = open('train.csv','w+')
 patient_train_list = os.listdir(train_path)
 f1.write('t1,t1_bfc,t2, t1ce, t1ce_bfc, flair, flair_bfc, gt\n')
+# This is the loop to make the csv file with the list of all the training examples
 for patient in patient_train_list:
     f1 = open("train.csv", 'a')
     with f1:
@@ -19,6 +20,7 @@ for patient in patient_train_list:
                          train_path + patient + '/' + patient + "_flair.nii.gz",
                          train_path + patient + '/' + patient + "_flair_bfc.nii.gz",
                          train_path + patient + '/' + patient + "_seg.nii.gz"])
+#This part of the code splits the entire data into 5 folds randomly and creates 10 csvs as given in the description
 df = pd.read_csv("train.csv")
 df = df.sample(frac = 1)
 df1 = df.iloc[0:67, :]
@@ -46,5 +48,3 @@ df_v2.to_csv("validation_fold2.csv", index = False)
 df_v3.to_csv("validation_fold3.csv", index = False)
 df_v4.to_csv("validation_fold4.csv", index = False)
 df_v5.to_csv("validation_fold5.csv", index = False)
-
-
