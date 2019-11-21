05e24566915e87c744cb9ccc415bc5418ee4316c
Megh Bhalerao Megh.Bhalerao@Pennmedicine.upenn.edu <bhaleram@cubic-login4.bicic.local>
2019-11-21 15:26:29 -0500

    Model Changes

--- gen_seg/Post_Process/seg_modules.py
+++ gen_seg/Post_Process/seg_modules.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class in_conv(nn.Module):
     def __init__(self, input_channels, output_channels, kernel_size = 3, 
                  dropout_p=0.3, leakiness=1e-2, conv_bias=True, 
@@ -111,9 +112,9 @@ class DownsamplingModule(nn.Module):
         self.in_0 = nn.InstanceNorm3d(output_channels, 
                                     affine=self.inst_norm_affine,
                                     track_running_stats=True)
-        self.conv0 = nn.Conv3d(input_channels, output_channels, kernel_size=3,
+        self.conv0 = nn.Conv3d(input_channels, output_channels, kernel_size = 3,
                                stride=2, padding=(kernel_size - 1) // 2, 
-                               bias=self.conv_bias)
+                               bias = self.conv_bias)
 
     def forward(self, x):
         """[This is a forward function for ]
@@ -432,6 +433,140 @@ class out_conv(nn.Module):
         if self.res == True:
             x = x + skip
         x = F.leaky_relu(self.in_3(x))
-        x = F.softmax(self.conv3(x), dim = 1)
-        #x = F.sigmoid(self.conv3(x))
+        x = F.softmax(self.conv3(x),dim=1)
+        return x
+ 
+#the operations in the initialization functions just define these functions and then they are used in the forward method 
+#Here the number of input and output channels are the same as this is the encoding inception module - in the decoding inception module they will differ - input # of features will be twice the output # of features
+
+class InceptionModule(nn.Module):
+    def __init__(self,input_channels,output_channels,dropout_p=0.3,leakiness=1e-2,conv_bias=True,inst_norm_affine=True,res=False,lrelu_inplace=True):
+        nn.Module.__init__(self)
+        self.res = res
+        self.output_channels = output_channels
+        self.dropout_p = dropout_p
+        self.conv_bias = conv_bias
+        self.leakiness = leakiness
+        self.inst_norm_affine = inst_norm_affine
+        self.lrelu_inplace = lrelu_inplace
+        self.dropout = nn.Dropout3d(dropout_p)
+        self.inst_norm = nn.InstanceNorm3d(int(output_channels/4),affine = self.inst_norm_affine, track_running_stats = True)
+        self.inst_norm_final = nn.InstanceNorm3d(output_channels,affine = self.inst_norm_affine, track_running_stats = True)
+        self.conv_1x1 = nn.Conv3d(output_channels,int(output_channels/4),kernel_size = 1,stride=1,padding=0,bias = self.conv_bias)
+        self.conv_3x3 = nn.Conv3d(int(output_channels/4),int(output_channels/4),kernel_size=3,stride=1,padding=1,bias=self.conv_bias)
+        self.conv_1x1_final = nn.Conv3d(output_channels,output_channels,kernel_size = 1, stride = 1, padding=0,bias = self.conv_bias)
+        
+    def forward(self,x):
+        output_channels = self.output_channels
+        if self.res == True:
+            skip = x
+        x1 = F.leaky_relu(self.inst_norm(self.conv_1x1(x)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        
+        x2 = F.leaky_relu(self.inst_norm(self.conv_1x1(x)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x2 = F.leaky_relu(self.inst_norm(self.conv_3x3(x2)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        
+        
+        x3 = F.leaky_relu(self.inst_norm(self.conv_1x1(x)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x3 = F.leaky_relu(self.inst_norm(self.conv_3x3(x3)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x3 = F.leaky_relu(self.inst_norm(self.conv_3x3(x3)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        
+        x4 = F.leaky_relu(self.inst_norm(self.conv_1x1(x)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x4 = F.leaky_relu(self.inst_norm(self.conv_3x3(x4)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x4 = F.leaky_relu(self.inst_norm(self.conv_3x3(x4)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x4 = F.leaky_relu(self.inst_norm(self.conv_3x3(x4)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        
+        x = torch.cat((x1,x2,x3,x4),dim=1)
+        x = self.inst_norm_final(self.conv_1x1_final(x))
+        
+        x = x + skip
+        x = F.leaky_relu(x,negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        
+        return x 
+
+class ResNetModule(nn.Module):
+    def __init__(self,input_channels,output_channels,dropout_p=0.3,leakiness=1e-2,conv_bias=True,inst_norm_affine=True,res=False,lrelu_inplace=True):
+        nn.Module.__init__(self)
+        self.output_channels = output_channels
+        self.dropout_p = dropout_p
+        self.leakiness = leakiness
+        self.conv_bias = conv_bias 
+        self.inst_norm_affine = inst_norm_affine
+        self.res = res
+        self.lrelu_inplace = lrelu_inplace 
+        self.dropout = nn.Dropout3d(dropout_p)
+        self.inst_norm = nn.InstanceNorm3d(output_channels,affine = self.inst_norm_affine, track_running_stats = True)
+        self.conv = nn.Conv3d(output_channels,output_channels,kernel_size=3,stride=1,padding=1,bias = self.conv_bias)
+    def forward(self,x):
+        if self.res == True:
+            skip = x          
+        x = F.leaky_relu(self.inst_norm(self.conv(x)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        x = self.inst_norm(self.conv(x))
+        x = x + skip
+        x = F.leaky_relu(x,negative_slope = self.leakiness, inplace = self.lrelu_inplace)
+        return x 
+    
+    
+class IncDownsamplingModule(nn.Module):
+    def __init__(self, input_channels, output_channels, leakiness=1e-2, kernel_size=1, conv_bias=True, 
+                 inst_norm_affine=True, lrelu_inplace=True):
+        nn.Module.__init__(self)
+        self.output_channels = output_channels 
+        self.input_channels = input_channels
+        self.leakiness = leakiness
+        self.conv_bias = conv_bias 
+        self.lrelu_inplace = lrelu_inplace
+        self.inst_norm_affine = inst_norm_affine
+        self.inst_norm = nn.InstanceNorm3d(output_channels,affine = self.inst_norm_affine, track_running_stats = True)
+        self.down = nn.Conv3d(input_channels,output_channels,kernel_size = 1, stride = 2, padding = 0, bias = self.conv_bias)
+    
+    def forward(self,x):
+        x = F.leaky_relu(self.inst_norm(self.down(x)),negative_slope = self.leakiness, inplace = self.lrelu_inplace)
+        return x
+    
+class IncConv(nn.Module):
+    def __init__(self,input_channels,output_channels,dropout_p=0.3,leakiness=1e-2,conv_bias=True,inst_norm_affine=True,res=False,lrelu_inplace=True):
+        nn.Module.__init__(self)
+        self.output_channels = output_channels 
+        self.leakiness = leakiness
+        self.conv_bias = conv_bias 
+        self.lrelu_inplace = lrelu_inplace
+        self.inst_norm_affine = inst_norm_affine
+        self.inst_norm = nn.InstanceNorm3d(output_channels,affine=self.inst_norm_affine,track_running_stats = True)
+        self.conv = nn.Conv3d(input_channels,output_channels,kernel_size=1,stride=1,padding=0,bias=self.conv_bias)
+    def forward(self,x):
+        x = F.leaky_relu(self.inst_norm(self.conv(x)),negative_slope = self.leakiness,inplace = self.lrelu_inplace)
+        return x
+
+class IncDropout(nn.Module):
+    def __init__(self,input_channels,output_channels,dropout_p=0.3,leakiness=1e-2,conv_bias=True,inst_norm_affine=True, res = False, lrelu_inplace = True):
+        nn.Module.__init__(self)
+        self.input_channels = input_channels
+        self.output_channels = output_channels
+        self.dropout_p = dropout_p
+        self.leakiness = leakiness
+        self.conv_bias = conv_bias 
+        self.inst_norm_affine = inst_norm_affine
+        self.res = res
+        self.lrelu_inplace = lrelu_inplace 
+        self.dropout = nn.Dropout3d(dropout_p)
+        self.conv = nn.Conv3d(input_channels,output_channels,kernel_size=1,stride=1,padding=0,bias=self.conv_bias)
+    def forward(self,x):
+        x = self.dropout(x)
+        x = self.conv(x)
+        return x
+class IncUpsamplingModule(nn.Module):
+    def __init__(self,input_channels,output_channels,dropout_p=0.3,leakiness=1e-2,conv_bias=True,inst_norm_affine=True, res = False, lrelu_inplace = True):
+        nn.Module.__init__(self)
+        self.input_channels = input_channels 
+        self.output_channels = output_channels
+        self.dropout_p = dropout_p
+        self.leakiness = leakiness
+        self.conv_bias = conv_bias 
+        self.inst_norm_affine = inst_norm_affine
+        self.res = res
+        self.lrelu_inplace = lrelu_inplace
+        self.inst_norm = nn.InstanceNorm3d(output_channels,affine = self.inst_norm_affine, track_running_stats = True)
+        self.up = nn.ConvTranspose3d(input_channels,output_channels,kernel_size=1,stride=2,padding=0,output_padding =1,bias = self.conv_bias)
+    def forward(self,x):
+        x = F.leaky_relu(self.inst_norm(self.up(x)),negative_slope=self.leakiness,inplace=self.lrelu_inplace)
         return x
