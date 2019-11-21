885dfbad96d1865882955f14c279a195f3a643ba
Megh Bhalerao Megh.Bhalerao@Pennmedicine.upenn.edu <bhaleram@cubic-login4.bicic.local>
2019-11-21 15:24:40 -0500

    Change of UInc Arch

--- gen_seg/gen_validation/new_models.py
+++ gen_seg/gen_validation/new_models.py
@@ -1,21 +1,13 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 19 15:08:03 2019
-
-@author: megh
-"""
-
 import torch.nn.functional as F
 import torch.nn as nn
 import torch
-from seg_modules import in_conv, DownsamplingModule, EncodingModule
-from seg_modules import UpsamplingModule, DecodingModule
-from seg_modules import out_conv, FCNUpsamplingModule
+from seg_modules import in_conv, DownsamplingModule, EncodingModule, InceptionModule, ResNetModule
+from seg_modules import UpsamplingModule, DecodingModule,IncDownsamplingModule,IncConv
+from seg_modules import out_conv, FCNUpsamplingModule, IncDropout,IncUpsamplingModule
 
 
 class unet(nn.Module):
-    def __init__(self, n_channels, n_classes, base_filters=30):
+    def __init__(self, n_channels, n_classes, base_filters):
         super(unet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
@@ -144,4 +136,63 @@ class fcn(nn.Module):
         u1 = self.us_0(x1)
         x = torch.cat([u5, u4, u3, u2, u1], dim=1)
         x = self.conv_0(x)
-        return F.sigmoid(x)
+        return F.softmax(x,dim=1)
+
+#defining the inception net class which inherits from the nn.module class - which is I think to basically tell the program that this is a pytorch network model that I am defining 
+#the res parameter is for the addition of the initial feature map with the final feature map after performance of the convolutions - for the decoding module, not the initial input but the input after the first convolution is addded to the final output since the initial input and the final one do not have the same dimensions
+#In the init function we just define the operations that are going to be performed in the forward pass of the network - and in the forward method we actually substitute the values in the defined operations
+        
+class uinc(nn.Module):
+    def __init__(self,n_channels,n_classes,base_filters):
+        super(uinc,self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.conv0_1x1 = IncConv(n_channels, base_filters)
+        self.rn_0 = ResNetModule(base_filters,base_filters,res=True)
+        self.ri_0 = InceptionModule(base_filters,base_filters,res=True)
+        self.ds_0 = IncDownsamplingModule(base_filters,base_filters*2)
+        self.ri_1 = InceptionModule(base_filters*2,base_filters*2,res=True)
+        self.ds_1 = IncDownsamplingModule(base_filters*2,base_filters*4)
+        self.ri_2 = InceptionModule(base_filters*4,base_filters*4,res=True)
+        self.ds_2 = IncDownsamplingModule(base_filters*4,base_filters*8)
+        self.ri_3 = InceptionModule(base_filters*8,base_filters*8,res=True)
+        self.ds_3 = IncDownsamplingModule(base_filters*8,base_filters*16)
+        self.ri_4 = InceptionModule(base_filters*16,base_filters*16,res=True)
+        self.us_3 = IncUpsamplingModule(base_filters*16,base_filters*8)
+        self.ri_5 = InceptionModule(base_filters*16,base_filters*16,res=True)
+        self.us_2 = IncUpsamplingModule(base_filters*16,base_filters*4)
+        self.ri_6 = InceptionModule(base_filters*8,base_filters*8,res=True)
+        self.us_1 = IncUpsamplingModule(base_filters*8,base_filters*2)
+        self.ri_7 = InceptionModule(base_filters*4,base_filters*4,res=True)
+        self.us_0 = IncUpsamplingModule(base_filters*4,base_filters)
+        self.ri_8 = InceptionModule(base_filters*2,base_filters*2,res=True)
+        self.conv9_1x1 = IncConv(base_filters*2,base_filters)
+        self.rn_10 = ResNetModule(base_filters*2,base_filters*2,res=True)
+        self.dropout = IncDropout(base_filters*2,n_classes)
+    
+    def forward(self,x):
+
+        x = self.conv0_1x1(x)
+        x1 = self.rn_0(x)
+        x2 = self.ri_0(x1)
+        x3 = self.ds_0(x2)
+        x3 = self.ri_1(x3)
+        x4 = self.ds_1(x3)
+        x4 = self.ri_2(x4)
+        x5 = self.ds_2(x4)
+        x5 = self.ri_3(x5)
+        x6 = self.ds_3(x5)
+        x6 = self.ri_4(x6)
+        x6 = self.us_3(x6)
+        x6 = self.ri_5(torch.cat((x5,x6),dim=1))
+        x6 = self.us_2(x6)
+        x6 = self.ri_6(torch.cat((x4,x6),dim=1))
+        x6 = self.us_1(x6)
+        x6 = self.ri_7(torch.cat((x3,x6),dim=1))
+        x6 = self.us_0(x6)
+        x6 = self.ri_8(torch.cat((x2,x6),dim=1))
+        x6 = self.conv9_1x1(x6)
+        x6 = self.rn_10(torch.cat((x1,x6),dim=1))
+        x6 = self.dropout(x6)
+        x6 = F.softmax(x6,dim=1)
+        return x6
